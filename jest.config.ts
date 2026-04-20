6c5c55ee9e09f95742067dec7950b37f97cd9ad1
vinjadevix <vinjatovix@gmail.com>
2026-04-20 16:07:19 +0200

    fix: update Jest configuration for coverage reporting and improve test scripts

--- jest.config.ts
+++ jest.config.ts
@@ -3,33 +3,32 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+
   setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
+
   transform: {
-    '^.+\\.tsx?$': ['ts-jest', { tsconfig: { module: 'CommonJS', moduleResolution: 'node', ignoreDeprecations: '6.0' } }]
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: {
+          module: 'CommonJS',
+          moduleResolution: 'node',
+          ignoreDeprecations: '6.0'
+        }
+      }
+    ]
   },
+
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1'
   },
+
   cacheDirectory: '.tmp/jestCache',
 
-  collectCoverage: true,
-  coverageDirectory: '<rootDir>/coverage/jest',
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageReporters: ['lcov'],
-  coveragePathIgnorePatterns: [
-    '/interfaces/',
-    '/routes/',
-    '/controllers/',
-    '/dist/',
-    '/node_modules/',
-    '/test/',
-    '/tests/',
-    '/index.ts',
-    '/server.ts'
-  ],
+  collectCoverage: false,
 
   watchPathIgnorePatterns: ['<rootDir>/test-report.json'],
-  // testResultsProcessor: 'jest-sonar-reporter', // si usas Sonar
+
   maxWorkers: '50%'
 };
 
