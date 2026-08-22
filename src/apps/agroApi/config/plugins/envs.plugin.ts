13f901f0e46807e534d29612a38d5583aaad32e0
vinjatovix <vinjatovix@gmail.com>
2026-08-23 01:58:27 +0200

    feat: add .env.test configuration and update dotenv integration for test environment fix: prevent MongoClientFactory from connecting to Mongo Atlas in test environment

--- src/apps/agroApi/config/plugins/envs.plugin.ts
+++ src/apps/agroApi/config/plugins/envs.plugin.ts
@@ -1,5 +1,9 @@
-import 'dotenv/config';
+import dotenv from 'dotenv';
 import env from 'env-var';
+
+dotenv.config({
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
+});
 
 export const envs = {
   PORT: env.get('PORT').required().asPortNumber(),
